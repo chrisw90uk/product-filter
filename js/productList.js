7f9431239692a05f0694c06c83e8d431bde559a0
Chris Wickham <chrisw90_uk@hotmail.com>
2017-04-11 22:59:59 +0100

    Changed checkboxes into array, rewrote functions to detect checkbox changes, reset checkboxes and apply a filter on ng-repeat

--- js/productList.js
+++ js/productList.js
@@ -1,38 +1,66 @@
 app.controller('productList',
 	function($scope){
+		$scope.categories = [
+			{name: "cinematic", selected: false},
+			{name: "atmospheric", selected: false},
+			{name: "world", selected: false}
+		]
 		$scope.products = [
 			{
 				name: "Orchestral Essentials",
 				description: "Lorem ipsum text",
-				categories: "cinematic",
+				categories: ["cinematic"],
 				image: "img/orchestral-essentials.jpg"
 			},
 			{
 				name: "Kinetic Treats",
 				image: "img/kinetic-treats.jpg",
 				description: "Lorem ipsum text",
-				categories: "atmospheric"
+				categories: ["atmospheric","world"]
 			},
 			{
 				name: "Juggernaut",
 				image: "img/juggernaut.png",
 				description: "Lorem ipsum text",
-				categories: "world"
+				categories: ["world","cinematic"]
 			}
 		];
 		
-		$scope.filtersApplied = filtersApplied = [];
-		$scope.categoryToFilter = function(cat){
-			if(filtersApplied.indexOf(cat)==-1){
-				filtersApplied.push(cat);
-			}else{
-				filtersApplied.splice(filtersApplied.indexOf(cat),1);
+		$scope.showAll = true;
+		
+		$scope.applyFilter = function() {
+			for(category in $scope.categories){
+				if($scope.categories[category].selected){
+					$scope.showAll = false;
+					return;
+				}
+			}
+			$scope.showAll = true;
+		};
+		
+		
+		
+		$scope.appliedFilters = function(product){
+			if ($scope.showAll){
+				return true;
+			}
+			for(category in $scope.categories){
+				var currentCat = $scope.categories[category];
+				if(currentCat.selected){
+					if(product.categories.indexOf(currentCat.name) != -1){
+						return true;
+					}
+				}
 			}
 		}
 		
-		$scope.removeFilters = function(){
-			document.querySelector('input[name="category"]').checked = false;
-			filtersApplied.length = 0;
+		$scope.resetFilters = function(){
+			
+			for(category in $scope.categories){
+				var currentCat = $scope.categories[category];
+				currentCat.selected = false;
+			}
+			$scope.showAll = true;
 		}
 	}
 );
