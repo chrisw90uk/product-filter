17b8e548e5794a3016915ea01de887ab3f57c68f
Chris Wickham <chrisw90_uk@hotmail.com>
2017-04-14 10:04:41 +0100

    Commit before merge

--- js/productList.js
+++ js/productList.js
@@ -1,32 +1,41 @@
 app.controller('productList',
 	function($scope){
 		$scope.categories = [
-			{name: "cinematic", selected: false},
-			{name: "atmospheric", selected: false},
-			{name: "world", selected: false}
+			{name: "Cinematic", selected: false},
+			{name: "Atmospheric", selected: false},
+			{name: "Electronic", selected: false},
+			{name: "Jazz", selected: false},
 		]
 		$scope.products = [
 			{
 				name: "Orchestral Essentials",
-				description: "Lorem ipsum text",
-				categories: ["cinematic"],
+				description: "A comprehensive pack of realistic orchestral instruments",
+				categories: ["Cinematic"],
 				image: "img/orchestral-essentials.jpg"
 			},
 			{
 				name: "Kinetic Treats",
 				image: "img/kinetic-treats.jpg",
-				description: "Lorem ipsum text",
-				categories: ["atmospheric","world"]
+				description: "Weird and wonderful sounds",
+				categories: ["Atmospheric"]
 			},
 			{
 				name: "Juggernaut",
 				image: "img/juggernaut.png",
-				description: "Lorem ipsum text",
-				categories: ["world","cinematic"]
+				description: "Percussive hits and evolving pads",
+				categories: ["Electronic","Cinematic"]
+			},
+			{
+				name: "Session Horns",
+				image: "img/session-horns.jpg",
+				description: "Brass instruments for pop and jazz",
+				categories: ["Jazz","Cinematic"]
 			}
 		];
 		
 		$scope.showAll = true;
+		
+		//this is mostly to estabish whether to show all or not
 		
 		$scope.applyFilter = function() {
 			for(category in $scope.categories){
@@ -40,14 +49,14 @@ app.controller('productList',
 		
 		
 		
-		$scope.appliedFilters = function(product){
+		$scope.appliedFilters = function(currentProduct){
 			if ($scope.showAll){
 				return true;
 			}
 			for(category in $scope.categories){
 				var currentCat = $scope.categories[category];
 				if(currentCat.selected){
-					if(product.categories.indexOf(currentCat.name) != -1){
+					if(currentProduct.categories.indexOf(currentCat.name) != -1){
 						return true;
 					}
 				}
